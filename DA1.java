cc57ca549feaae38c45893a47738915d2c5b98f3
sahasrareddydsr <sahasrareddydondapati@gmail.com>
2026-01-12 21:50:38 +0530

    Add files via upload
    Initial commit - Java program for DA1

---
+++ DA1.java
@@ -0,0 +1,7 @@
+public class DA1 {
+    public static void main(String[] args) {
+        System.out.println("Student Name: SAHASRA REDDY");
+        System.out.println("Register Number: 23MIC0037");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
